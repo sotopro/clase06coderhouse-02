adf5bd9b78a7d87a743874a4679fac7d35e09275
dsoto1-rbi <dsoto1@rbi.com>
2022-06-06 18:56:38 -0500

    [feature] - create header component & implement module pattern for components

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, SafeAreaView } from "react-native";
+import { Header } from "./components/index";
+import { styles } from "./styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Header title="Adivina el número" />
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/header/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 96,
+    backgroundColor: "#D9DBF1",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#212121",
+    fontSize: 20,
+  },
+});
